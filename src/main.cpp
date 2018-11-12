a8757c8433205cf652a71d013548c1a3ff6a40d6
Daria2002 <daria.matkovic@fer.hr>
2018-11-12 19:51:15 +0100

    Added CMakeLists

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "bf/bloom_filter.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char *argv[]) 
+{
+	if(argc != 2)
+		cerr << "Please write two arguments: path to fasta file an number of k-mers"
+	K = argv[1]
+	pathToFastaFile = argv[2]
+    cerr << "Classic Bloom Filter" << endl;
+    
+} 
